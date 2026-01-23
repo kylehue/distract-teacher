0eb62726548991479d93e76fc30c245245921863
kai <apeuda69@gmail.com>
2026-01-23 15:22:34 +0800

    fix: fix bg and overflow

--- src/app/pages/layout.vue
+++ src/app/pages/layout.vue
@@ -1,46 +1,54 @@
 <template>
-   <div class="flex flex-col w-screen h-screen">
-      <nav>
-         <div class="container mx-auto h-16 flex items-center justify-between">
-            <RouterLink to="/">
-               <NText>Distract</NText>
-            </RouterLink>
-            <div class="flex items-center">
-               <template v-for="link in links" :key="link.to">
-                  <RouterLink v-if="!link.isHidden" :to="link.to" class="px-2">
-                     <NButton
-                        :secondary="link.isActive"
-                        :quaternary="!link.isActive"
-                        :focusable="false"
-                        size="small"
+   <NLayout embedded>
+      <div class="flex flex-col w-screen h-screen">
+         <nav>
+            <div
+               class="container mx-auto h-16 flex items-center justify-between"
+            >
+               <RouterLink to="/">
+                  <NText>Distract</NText>
+               </RouterLink>
+               <div class="flex items-center">
+                  <template v-for="link in links" :key="link.to">
+                     <RouterLink
+                        v-if="!link.isHidden"
+                        :to="link.to"
+                        class="px-2"
                      >
-                        <NText :depth="link.isActive ? 1 : 3">
-                           {{ link.label }}
-                        </NText>
-                     </NButton>
-                  </RouterLink>
-               </template>
-               <div v-if="auth.isAuthenticated" class="flex items-center">
-                  <NDivider vertical class="mx-4!" />
-                  <NPopselect
-                     trigger="click"
-                     :options="userMenuOptions"
-                     @update:value="onUpdateUserMenu"
-                  >
-                     <NButton circle>
-                        <template #icon>
-                           <PhUser />
-                        </template>
-                     </NButton>
-                  </NPopselect>
+                        <NButton
+                           :secondary="link.isActive"
+                           :quaternary="!link.isActive"
+                           :focusable="false"
+                           size="small"
+                        >
+                           <NText :depth="link.isActive ? 1 : 3">
+                              {{ link.label }}
+                           </NText>
+                        </NButton>
+                     </RouterLink>
+                  </template>
+                  <div v-if="auth.isAuthenticated" class="flex items-center">
+                     <NDivider vertical class="mx-4!" />
+                     <NPopselect
+                        trigger="click"
+                        :options="userMenuOptions"
+                        @update:value="onUpdateUserMenu"
+                     >
+                        <NButton circle>
+                           <template #icon>
+                              <PhUser />
+                           </template>
+                        </NButton>
+                     </NPopselect>
+                  </div>
                </div>
             </div>
+         </nav>
+         <div class="container mx-auto flex flex-col flex-1">
+            <RouterView></RouterView>
          </div>
-      </nav>
-      <div class="container mx-auto flex flex-col flex-1">
-         <RouterView></RouterView>
       </div>
-   </div>
+   </NLayout>
 </template>
 
 <script setup lang="ts">
